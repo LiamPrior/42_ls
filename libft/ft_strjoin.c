13ef3fb97d8ee0d8087aca1cdf80ced6f8f5b174
LiamPrior <Liamprior44@gmail.com>
2018-04-21 23:35:11 -0700

    had to fix the NULL

--- libft/ft_strjoin.c
+++ libft/ft_strjoin.c
@@ -6,7 +6,7 @@
 /*   By: lprior <lprior@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/09/27 13:33:40 by lprior            #+#    #+#             */
-/*   Updated: 2018/04/20 20:50:42 by lprior           ###   ########.fr       */
+/*   Updated: 2018/04/21 22:37:29 by lprior           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -33,11 +33,13 @@ char *ft_strjoin2(char const *s1, char const *s2)//may not work with /
 	if (!s1 || !s2)
 		return (NULL);
 	if (!(str = (char *)malloc(ft_strlen((char *)s1)
-		+ ft_strlen((char *)s2) + 1)))
+		+ ft_strlen((char *)s2) + 2)))//member
 		return (NULL);
 	str = ft_strcpy(str, s1);
 	str = ft_strcat(str, s2);
 	str[ft_strlen(str)] = '/';
+	str[ft_strlen(str) + 1] = '\0';
+
 	// free(s1);
 	// printf("in strjoin[%s]\n", str);
 	return (str);
